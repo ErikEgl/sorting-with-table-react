18b58505ace39d240f58559bb5b8f8a93bad0955
Erik <Mr.erik.egliens@gmail.com>
2022-07-02 00:15:40 +0300

    formating

--- src/components/Cards/index.js
+++ src/components/Cards/index.js
@@ -1,16 +1,38 @@
+import React from "react";
 import CheckboxCard from "./CheckboxCard/CheckboxCard";
 import data from "../../data/data";
 
-function CheckboxCards(props) {
+function CheckboxCards(props) { 
   const cardItem = data.map((item) => {
-    return <CheckboxCard iCounter={props.iCounter} weeksCounter={props.weeksCounter} mondayCounter={props.mondayCounter} monthCounter={props.monthCounter} key={item.id} {...item} />;
+    return (
+      <CheckboxCard
+        iCounter={props.iCounter}
+        weeksCounter={props.weeksCounter}
+        mondayCounter={props.mondayCounter}
+        monthCounter={props.monthCounter}
+        key={item.id}
+        {...item}
+      />
+    );
   });
+  const [chooseTable, setChooseTable] = React.useState(false);
+  function handleClick () {
+    setChooseTable(prevState => !prevState)
+  }
+  const styles = {
+    outline: chooseTable ? "2px solid green" : "none"
+  }
   return (
     <>
-      <td className="mw-140">
-        <small className="date-title">Нед. <time>{props.weeksCounter}</time> от <time>{props.mondayCounter} {props.monthCounter}</time></small >
-          {cardItem}
-        </td>
+      <td onClick={handleClick} style={styles} className="mw-140">
+        <small className="date-title">
+          Нед. <time>{props.weeksCounter}</time> от{" "}
+          <time>
+            {props.mondayCounter} {props.monthCounter}
+          </time>
+        </small>
+        {cardItem}
+      </td>
     </>
   );
 }
